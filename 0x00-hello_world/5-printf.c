5968d60be5043988c30abd1dbf81741605bbd5c8
Holden Grissett <holdeng96@gmail.com>
2016-10-12 22:17:58 +0000

    Betty-style/doc add to 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints the required string
+ * Return: 0
+ */
+int main(void)
 {
-  printf("with proper grammer, but the outcome is a piece of art,\n");
-  return 0;
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
