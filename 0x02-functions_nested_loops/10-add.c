7eb8b83db8922ea937d9e60d894e9eb3116fa344
Holden Grissett <holdeng96@gmail.com>
2016-10-19 00:12:32 +0000

    add files for 10: 10-add.c, 10-main.c. Append prototype 10 to holberton.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers and return result
+ *
+ * @a: first integer to be added
+ *
+ * @b: second integer to be added to @a
+ *
+ * Return: result of added integers
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
