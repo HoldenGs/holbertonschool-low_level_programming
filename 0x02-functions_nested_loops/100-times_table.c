6d77559afb82a08570e0b7fed52e6309929450dd
Holden Grissett <holdeng96@gmail.com>
2016-10-19 01:51:45 +0000

    fix for 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,11 @@
 void print_times_table(int n)
 {
 	int i, j;
-
-	if (n < 15 && n > 0)
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+	} else if (n < 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
