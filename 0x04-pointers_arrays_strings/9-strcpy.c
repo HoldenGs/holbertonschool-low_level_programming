c5eed7696d00b0037dc040567a7238a7f55b4166
Holden Grissett <holdeng96@gmail.com>
2016-10-25 22:59:18 +0000

    add files for #9

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * strcpy - copies @src string to @dest buffer, including '\0'
+ *
+ * @dest: pointer to buffer for string
+ *
+ * @src: pointer to string to copy to buffer
+ *
+ * Return: @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+
+	while (*(src + n))
+	{
+		*(dest + n) = *(src + n);
+		n++;
+	}
+	*(dest + (n + 1)) = '\0';
+	return dest;
+}
