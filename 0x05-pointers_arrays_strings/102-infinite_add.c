4ea762eed61707940ba72fa3cfdd9b43e4686857
Holden Grissett <holdeng96@gmail.com>
2016-10-28 20:32:06 +0000

    add changes

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - add two numbers
+ *
+ * @n1: first number to add
+ * @n2: second number to add
+ * @r: buffer to store sum
+ * @size_r: size of buffer
+ *
+ * Return: pointer to buffer @r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j;
+
+	while (
+}
