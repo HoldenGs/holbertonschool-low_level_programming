bb8efa2b08b2faf3a8974aec6c4840443e6b3bfc
Holden Grissett <holdeng96@gmail.com>
2016-10-28 05:59:52 +0000

    add files for #9

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
-<<<<<<< HEAD
 #include <limits.h>
-=======
->>>>>>> 2072bcd83d112d360e2e604254f30a2acc9de21e
+
+
 
 /**
  * main - check the code for Holberton School students.
@@ -11,11 +10,8 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	print_number(INT_MIN);
 	_putchar('\n');
-=======
->>>>>>> 2072bcd83d112d360e2e604254f30a2acc9de21e
 	print_number(98);
 	_putchar('\n');
 	print_number(402);
--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -9,7 +9,6 @@
  */
 void print_number(int n)
 {
-<<<<<<< HEAD
 	int length = 1000000000, negative = 0;
 
 	if (n > 0)
@@ -37,25 +36,4 @@ void print_number(int n)
 	}
 	else
 		_putchar(n + '0');
-=======
-	int length, tmp = n;
-
-	if (n == 0)
-		_putchar(n + '0');
-	else if (n < 0)
-		_putchar('-');
-
-	while (tmp > 0)
-	{
-		if (tmp % 10 == 0)
-		tmp /= 10;
-		length++;
-	}
-
-	while (n != 0)
-	{
-		_putchar(n / length + '0');
-		length--;
-	}
->>>>>>> 2072bcd83d112d360e2e604254f30a2acc9de21e
 }
