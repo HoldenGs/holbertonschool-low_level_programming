ff16153b6653b15c4a24266aa561fdeff721426e
Holden Grissett <holdeng96@gmail.com>
2016-11-02 18:33:36 +0000

    add init files and #0 files

---
+++ 0x07-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_puts_recursion("Betty Holberton");
+	return (0);
+}
