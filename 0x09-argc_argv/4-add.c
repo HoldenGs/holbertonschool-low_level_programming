d4210eaa549428134f78d32d9680df123813f9dc
Holden Grissett <holdeng96@gmail.com>
2016-11-07 06:48:16 +0000

    add 4-add.c

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add integer arguments together and print sum, followed by a newline
+ *
+ * @argc: number of arguments
+ * @argv: pointer array of arguments
+ *
+ * Return: 1 if there is a non-integer argument, otherwise 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, count;
+
+	sum = 0;
+	for (count = 1; count < argc; count++)
+	{
+		num = atoi(argv[count]);
+		if (!num || num < 0)
+		{
+			printf("Error\n");
+			return (0);
+		}
+		else
+			sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
