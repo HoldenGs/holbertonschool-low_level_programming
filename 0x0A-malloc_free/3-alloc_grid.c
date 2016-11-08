08a910692a3b8fa17c13b60edfbd4d720c1a1822
Holden Grissett <holdeng96@gmail.com>
2016-11-08 22:55:33 +0000

    add file 3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - create a new 2D integer array initialized with 0's
+ *
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: pointer to array, otherwise NULL if insufficient memory
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a, i, j;
+
+	a = (int **)malloc(sizeof(int *) * height);
+	a[0] = (int *)malloc(sizeof(int) * width * height);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+		a[i] = (*a + width * i);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			a[i][j] = 0;
+
+	return (a);
+}
