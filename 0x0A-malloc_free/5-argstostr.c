e34903f8c30dd356f7f2aee509400acbb8bc4d69
Holden Grissett <holdeng96@gmail.com>
2016-11-09 01:45:33 +0000

    add file 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenate all the arguments passed to the program
+ *
+ * @ac: number of arguments
+ * @av: pointer array of arguments
+ *
+ * Return: pointer to new string; NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	char *a;
+	int i, j, size, strcount;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
+			size++;
+		size++;
+	}
+
+	a = malloc((size + 1) * sizeof(char));
+
+	if (a == NULL)
+		return (NULL);
+
+	strcount = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
+		{
+			*(a + strcount) = *(*(av + i) + j);
+			strcount++;
+		}
+		*(a + strcount) = '\n';
+		strcount++;
+	}
+	*(a + strcount) = '\0';
+
+	return (a);
+}
