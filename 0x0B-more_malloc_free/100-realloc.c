2d0b75edb8eb272446791f19e12cf81b73986084
Holden Grissett <holdeng96@gmail.com>
2016-11-10 23:24:43 +0000

    possible fixes

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -27,6 +27,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	else if (old_size == 0)
+	{
+		p = malloc(new_size);
+		free(ptr);
+		return (p);
+	}
 
 	a = malloc(new_size);
 	if (a == NULL)
