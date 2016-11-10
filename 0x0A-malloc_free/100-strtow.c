9e6736b95c54c67aeb1fbbe950b92acfd1360e27
Holden Grissett <holdeng96@gmail.com>
2016-11-10 03:41:35 +0000

    add 100

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -12,54 +12,131 @@
 char **strtow(char *str)
 {
 	char **a;
-	int i, j, k, wc, cc;
+	int j, k, wc, cc, inword;
+
+	inword = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	putchar('a');
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		putchar('b');
-		if (str[i] == ' ' && str[i - 1] != ' ' && i > 0)
-			wc++;
-	}
-
-	printf("%d", wc);
+	wc = word_count(str);
 
 	a = malloc(wc * sizeof(char *));
 
 	if (a == NULL)
 		return (NULL);
 
-	i = 0;
-	for (j = 0; j <= wc; j++)
+	for (j = 0; j < 3; j++)
 	{
-		putchar('d');
-		cc = 0;
-		while (str[i] != '\0')
-		{
-			if (str[i] != ' ')
-				cc++;
-			else if (str[i] == ' ' && str[i - 1] != ' ' && i > 0)
-				break;
-			i++;
-		}
-		i++;
-
+		cc = char_count(str, k);
 		cc++;
 		a[j] = malloc(cc * sizeof(char));
 
 		if (a[j] == NULL)
 			return (NULL);
 
-		for (k = 0; k < cc; k++)
+		k = wc = cc = 0;
+		while (str[k] != '\0')
 		{
-			putchar('E');
-			a[j][k] = str[i - cc + k + 1];
+			if (str[k] == ' ' && wc == j + 1)
+				break;
+			else if (str[k] == ' ')
+			{
+				inword = 0;
+				while (str[k] == ' ')
+					k++;
+			}
+			else
+			{
+				if (!inword && wc == j)
+				{
+					inword = 1;
+					wc++;
+				}
+				else if (!inword)
+				{
+					inword = 1;
+					wc++;
+				}
+				if (wc == j + 1)
+				{
+					a[j][cc] = str[k];
+					cc++;
+				}
+				k++;
+			}
 		}
-		str[i - cc + k + 1] = '\0';
+		a[j][cc] = '\0';
 	}
 
 	return (a);
+}
+
+/**
+ * word_count - count the words in string @s
+ *
+ * @s: string
+ *
+ * Return: integer word count, or NULL if failure
+ */
+int word_count(char *s)
+{
+	int i, inword, wc;
+
+	inword = i = wc = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ')
+		{
+			inword = 0;
+			while (s[i] == ' ')
+				i++;
+		}
+		else
+		{
+			if (inword == 0)
+			{
+				inword = 1;
+				wc++;
+			}
+			i++;
+		}
+	}
+
+	return (wc);
+}
+
+/**
+ * char_count - count the number of characters in the next word
+ *
+ * @s: string
+ * @start: starting position in string @s
+ *
+ * Return: number of characters in next word in string @s
+ */
+int char_count(char *s, int start)
+{
+	int i, cc, kill;
+
+	i = start;
+	cc = kill = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' && kill == 1)
+			return (cc);
+		else if (s[i] == ' ')
+		{
+			while (s[i] == ' ')
+				i++;
+		}
+		else
+		{
+			cc++;
+			i++;
+			kill = 1;
+		}
+	}
+	return (cc);
 }
