72cc415d3ff6df482094eec5ef22c25609781647
Holden Grissett <holdeng96@gmail.com>
2016-11-14 21:01:40 +0000

    change mechanism for 3

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,3 +1,16 @@
 #ifndef ABS
-#define ABS(x) ((x) < 0 ? -(x) : x)
+#define ABS(x) abs(x)
+/**
+ * abs - get absolute value of @x
+ *
+ * @x: value
+ *
+ * Return: absolute value of @x
+ */
+int abs(int x)
+{
+	if ((x) > 0)
+		return (-x);
+	return (x);
+}
 #endif
