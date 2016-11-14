a3a908e7a50c830d73f8dac644195ac63fa03c57
Holden Grissett <holdeng96@gmail.com>
2016-11-14 21:03:01 +0000

    revert 3

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,16 +1,3 @@
 #ifndef ABS
-#define ABS(x) abs(x)
-/**
- * abs - get absolute value of @x
- *
- * @x: value
- *
- * Return: absolute value of @x
- */
-int abs(int x)
-{
-	if ((x) > 0)
-		return (-x);
-	return (x);
-}
+#define ABS(x) ((x) > 0 ? -x : x)
 #endif
