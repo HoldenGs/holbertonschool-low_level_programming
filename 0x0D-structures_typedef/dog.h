f265b9cfca501c86c3820c2fe04231ec2acb109b
Holden Grissett <holdeng96@gmail.com>
2016-11-15 01:00:05 +0000

    add dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -10,13 +10,16 @@
  * Description: structure defining a dog with the @name of the dog, @age of
  * the dog, and the @owner name of the dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
