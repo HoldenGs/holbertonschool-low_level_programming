fac90772fd9356d8a1e2fd25324988e486c27ad0
Holden Grissett <holdeng96@gmail.com>
2016-11-15 00:47:47 +0000

    add dog_t alias for dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: structure defining a dog with the @name of the dog, @age of
  * the dog, and the @owner name of the dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
