7e7d74b770da1c1cc8ee1806d9ac5078245b8ab9
Holden Grissett <holdeng96@gmail.com>
2016-11-16 22:46:00 +0000

    fix NULL error

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -10,7 +10,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
