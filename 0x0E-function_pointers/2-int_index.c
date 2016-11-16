2b5c3d431dcc9c013fbf6b4cf19bf883eae6f95d
Holden Grissett <holdeng96@gmail.com>
2016-11-16 21:43:00 +0000

    add file 2

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+/**
+ * int_index - search for an integer in an array using @cmp
+ * @array: array
+ * @size: size of array
+ * @cmp: function used to look for an integer
+ *
+ * Return: Index of @array where match is found; -1 if failure or none is found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
