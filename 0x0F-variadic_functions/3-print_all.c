7644afd4b33ca88ea45e5b7b9989e58825bfe220
Holden Grissett <holden.grissett@holbertonschool.com>
2016-11-20 02:30:15 +0000

    init file 3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - print anything
+ * @format: string pointer denoting each argument type
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *s;
+	va_list list;
+
+	i = 0;
+	va_start(list, format);
+	while (format[i] != 0)
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(list, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(list, double));
+			break;
+		case 's':
+			s = va_arg(list, char *);
+			if (s == NULL)
+				printf("(nil)");
+			printf("%s", s);
+		}
+
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+		     format[i] == 's') && format[i + 1] != 0)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+}
