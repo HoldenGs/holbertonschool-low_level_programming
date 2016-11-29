f8fa2dcb99b0f957aae0e6976739ae6ca1f0bff5
Holden Grissett <holdeng96@gmail.com>
2016-11-29 08:02:11 +0000

    fix file output

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,21 @@
-        SECTION .data           ; data section
-msg:	            db "Hello, Holberton",10, 0 ; the string to print, 10=cr
-len:	            equ $-msg			; "$" means "here"
-;;;   len is a value, not an address
+        extern	printf		; the C function, to be called
 
-	        SECTION .text	; code section
+	        section .data	; Data section, initialized variables
+msg:		db "Hello, Holberton", 0 ; C string needs 0
+fmt:	    db "%s", 10, 0          ; The printf format, "\n",'0'
 
-	        global main     ; make label available to linker
-main:
-	        mov     edx,len
-	        mov     ecx,msg
-	        mov     ebx,1
-	        mov     eax,4	; write sysout command to int 80 hex
-	        int     0x80	; interrupt 80 hex, call kernel
+	        section .text	; Code section.
 
-	        mov     ebx,0	; exit code
-	        mov     eax,1	; exit command to kernel
-	        int     0x80	; interrupt 80 hex, call kernel
+	        global main	; the standard gcc entry point
+main:				; the program label for the entry point
+	        push    rbp	; set up stack frame, must be alligned
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	        call    printf	; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
