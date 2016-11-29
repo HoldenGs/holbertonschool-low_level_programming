a00872a275d393de47e12fea8d9d70df1cefd440
Holden Grissett <holdeng96@gmail.com>
2016-11-29 00:35:36 +0000

    format in compliance w/ Betty

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -2,11 +2,12 @@
 
 void print_first(void) __attribute__ ((constructor));
 /**
- * print a string before the main function
+ * print_first - print a string before the main function
  *
  * Return: void
  */
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon");
+	printf(" my back!\n");
 }
