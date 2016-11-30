ef4eb8e8a348d63b24e8789246b822becc93fc7a
Holden Grissett <holdeng96@gmail.com>
2016-11-30 22:42:58 +0000

    add file 5

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a listint_t linked list and set the @head to NULL
+ * @head: linked list start point
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	*head = NULL;
+}
