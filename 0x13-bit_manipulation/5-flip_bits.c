55261de00428d3629679fb14f60d99f22d93236b
Holden Grissett <holdeng96@gmail.com>
2016-12-06 06:03:42 +0000

    add file 5

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - return the number of bits needed to flip from
+ * one number to another
+ * @n: number
+ * @m: new number
+ *
+ * Return: number of bits you'd need to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned 
+}
