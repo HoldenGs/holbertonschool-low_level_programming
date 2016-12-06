ffce8613a27c34e91620b0fd851aa3e2280c6144
Holden Grissett <holdeng96@gmail.com>
2016-12-06 05:15:43 +0000

    fix overflow from index

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -2,16 +2,18 @@
 
 /**
  * get_bit - return the value of a bit at index @index
+ * @n: number to find bit in
  * @index: index of bit
  *
  * Return: value of bit at @index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int compare, i;
+	unsigned long int compare, i, ind;
 
+	ind = index;
 	compare = 1;
-	for (i = 0; i < index; i++)
+	for (i = 0; i < ind; i++)
 		compare *= 2;
 	if (n & compare)
 		return (1);
