1759ec8e19fecb3c94b6559a07c63d6585041893
Holden Grissett <holdeng96@gmail.com>
2016-12-06 05:13:11 +0000

    add file 2

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_bit - return the value of a bit at index @index
+ * @index: index of bit
+ *
+ * Return: value of bit at @index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int compare, i;
+
+	compare = 1;
+	for (i = 0; i < index; i++)
+		compare *= 2;
+	if (n & compare)
+		return (1);
+	else
+		return (0);
+}
