216f91017852aba3269651de8a61a4bcfaa8413a
Holden Grissett <holdeng96@gmail.com>
2016-12-08 19:21:28 +0000

    add file 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,7 +1,5 @@
 #include "holberton.h"
 
-
-
 /**
  * maincp - copy a file from one name to another
  * @ac: number of arguments
@@ -9,7 +7,34 @@
  *
  * Return: 0 on success; 97 on
  */
-int maincp(int ac, char **av)
+int main(int ac, char **av)
 {
+	int fdr, fdw, rlength, wlength, rclose, wclose;
+	char buf[BUFFERSIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fdr = open(av[1], O_RDONLY);
+	if (fdr == -1)
+		dprintf(fdr, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fdw = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fdw == -1)
+		dprintf(fdr, "Error: Can't write to %s\n", av[2]), exit(99);
+	while (rlength)
+	{
+		rlength = read(fdr, buf, BUFFERSIZE);
+		if (rlength == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		wlength = write(fdw, buf, rlength);
+		if (wlength == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
+	rclose = close(fdr);
+	if (rclose == -1)
+		dprintf(rclose, "Error: Can't close fd %d\n", fdr), exit(100);
+	wclose = close(fdw);
+	if (wclose == -1)
+		dprintf(wclose, "Error: Can't close fd %d\n", fdw), exit(100);
+	return (0);
 }
