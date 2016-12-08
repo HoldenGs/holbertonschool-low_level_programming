a3d1e7d37d82b61663f2ff56ec603495c72c94f0
Holden Grissett <holdeng96@gmail.com>
2016-12-08 19:54:34 +0000

    add NULL checking

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -15,6 +15,10 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	if (av[1] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (av[2] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	fdr = open(av[1], O_RDONLY);
 	if (fdr == -1)
 		dprintf(fdr, "Error: Can't read from file %s\n", av[1]), exit(98);
@@ -27,9 +31,12 @@ int main(int ac, char **av)
 		rlength = read(fdr, buf, BUFFERSIZE);
 		if (rlength == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		wlength = write(fdw, buf, rlength);
-		if (wlength == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		if (rlength > 0)
+		{
+			wlength = write(fdw, buf, rlength);
+			if (wlength == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
 	}
 	rclose = close(fdr);
 	if (rclose == -1)
