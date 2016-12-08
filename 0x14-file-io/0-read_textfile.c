849ffae1dae70bb44363377337402b40abc5593a
Holden Grissett <holdeng96@gmail.com>
2016-12-08 02:06:05 +0000

    add files 0

---
+++ 0x14-file-io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - read a text file and print it to POSIX stdout
+ * @filename: name of file
+ * @letters: number of letters that should be printed
+ *
+ * Return: actual number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, length;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	length = read(fd, buffer, letters);
+	write(1, buffer, letters);
+	return (length);
+}
