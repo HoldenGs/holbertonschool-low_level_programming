08cfd7b42c47bcfce1e19b6aac2f276890b783c4
Holden Grissett <holdeng96@gmail.com>
2016-12-08 06:44:58 +0000

    remove redundant flag in open

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -9,11 +9,11 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i, out;
+	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		close(fd);
@@ -23,9 +23,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != 0; i++)
 			;
-		out = write(fd, text_content, i);
-		if (out == -1)
-			return (-1);
+		write(fd, text_content, i);
 	}
 	close(fd);
 	return (1);
