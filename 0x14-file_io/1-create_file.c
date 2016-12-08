7812b29477816c51d1721328479c6f6686417484
Holden Grissett <holdeng96@gmail.com>
2016-12-08 04:00:18 +0000

    close file on NULL content

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,7 +15,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 	for (i = 0; text_content[i] != 0; i++)
 		;
 	write(fd, text_content, i);
