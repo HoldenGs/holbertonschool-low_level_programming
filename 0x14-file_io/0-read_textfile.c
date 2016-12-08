fb91cf02a7acba5022219407b5ce939b7891f648
Holden Grissett <holdeng96@gmail.com>
2016-12-08 02:32:14 +0000

    fix messed up write

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	length = read(fd, buffer, letters);
-	write(1, length, length);
+	write(1, buffer, letters);
 	close(fd);
 	free(buffer);
 	return (length);
