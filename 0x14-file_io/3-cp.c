607a3b3cb8b2c9b1746cd23019ec7d462cf78f0f
Holden Grissett <holdeng96@gmail.com>
2016-12-08 07:05:43 +0000

    init file 3

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+
+
+/**
+ * maincp - copy a file from one name to another
+ * @ac: number of arguments
+ * @av: pointer to arguments
+ *
+ * Return: 0 on success; 97 on
+ */
+int maincp(int ac, char **av)
+{
+
+}
