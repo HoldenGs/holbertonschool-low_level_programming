0225a81be160929082fea832989b09d60b46faee
Holden Grissett <holdeng96@gmail.com>
2016-12-08 04:03:12 +0000

    check for NULL content

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,14 +14,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
+		for (i = 0; text_content[i] != 0; i++)
+			;
+		write(fd, text_content, i);
 	}
-	for (i = 0; text_content[i] != 0; i++)
-		;
-	write(fd, text_content, i);
 	close(fd);
 	return (1);
 }
