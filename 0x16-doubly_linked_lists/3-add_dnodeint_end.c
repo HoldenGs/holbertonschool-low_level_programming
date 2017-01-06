9e3618ff3abc4b9d5db4a60edab40e507eb13991
Holden Grissett <holdeng96@gmail.com>
2017-01-06 21:21:28 +0000

    add file 3:

---
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *walk;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head != NULL)
+	{
+		walk = *head;
+		if (walk != NULL)
+			while (walk->next != NULL)
+				walk = walk->next;
+		walk->next = new;
+		new->prev = walk;
+	}
+	else
+	{
+		*head = new;
+		new->prev = NULL;
+	}
+	return (new);
+}
