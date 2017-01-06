24cd6715494f5072e29e883eedeb7e59141d4b64
Holden Grissett <holdeng96@gmail.com>
2017-01-06 21:35:05 +0000

    add file 5

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; head != NULL; i++)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
+}
