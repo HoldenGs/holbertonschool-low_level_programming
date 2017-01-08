a67fe10dc7906728984816d3cf65ce01fcc21491
Holden Grissett <holdeng96@gmail.com>
2017-01-08 22:56:53 +0000

    fix names

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,14 +10,14 @@
  *
  * Return: new node; NULL if failure
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *walk;
 	unsigned int i;
 
-	if (*head != NULL)
+	if (*h != NULL)
 	{
-		walk = *head;
+		walk = *h;
 		for (i = 0; walk != NULL; i++)
 		{
 			if (i == idx)
@@ -42,7 +42,7 @@ dlistint_t *insert_dnodeint_at_idx(dlistint_t **head, unsigned int idx, int n)
 			walk = walk->next;
 		}
 		if (i == idx)
-			add_dnodeint_end(head, n);
+			add_dnodeint_end(h, n);
 	}
 	return (NULL);
 }
