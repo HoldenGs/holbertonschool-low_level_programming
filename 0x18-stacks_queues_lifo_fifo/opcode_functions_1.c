7ff467e44950b4d61bd515ad6a13f5c40463c353
Holden Grissett <holdeng96@gmail.com>
2017-01-29 01:04:22 +0000

    add swap and pop

--- 0x18-stacks_queues_lifo_fifo/opcode_functions_1.c
+++ 0x18-stacks_queues_lifo_fifo/opcode_functions_1.c
@@ -1,10 +1,11 @@
 #include "monty.h"
 
 /**
- * push - add a node to the start of a doubly linked list
+ * push_op - add a node to the start of a doubly linked list
  *
  * @head: list head
  * @n: integer data
+ * @line_number: the line of access
  *
  * Return: 0
  */
@@ -20,9 +21,10 @@ void push_op(stack_t **head, int n)
 }
 
 /**
- * pall - prints a doubly linked list
+ * pall_op - prints a doubly linked list
  *
- * @h: list head
+ * @head: list head
+ * @line_number: the line of access
  *
  * Return: void
  */
@@ -39,21 +41,73 @@ void pall_op(stack_t **head, unsigned int line_number)
 		node = node->next;
 	}
 }
-
+/**
+ * pint_op - prints first element of doubly linked list
+ *
+ * @head: list head
+ * @line_number: line of access
+ *
+ * Return: void
+ */
 void pint_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
+	if (*head == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
 }
-
+/**
+ * pop_op - removes top element of a doubly linked list
+ *
+ * @head: the list
+ * @line_number: line of access
+ *
+ * Return: void
+ *
+ */
 void pop_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
-}
+	stack_t *current;
 
+	if (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	else
+	{
+		printf("L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+}
+/**
+ * swap_op - swaps top two elements of doubly linked list
+ *
+ * @head: the list
+ * @line_number: line of access
+ *
+ * Return: void
+ */
 void swap_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
+	stack_t *temp, *hold;
+	int holder;
+
+	if (*head != NULL && (*head)->next != NULL)
+	{
+		temp = *head;
+		hold = temp->next;
+		holder = temp->n;
+		temp->n = hold->n;
+		hold->n = holder;
+	}
+	else
+	{
+		printf("L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
