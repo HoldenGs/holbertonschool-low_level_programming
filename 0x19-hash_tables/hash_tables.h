5f27be87edebcdab57c010453b90a4fcdb22bf92
Holden Grissett <holdeng96@gmail.com>
2017-02-02 22:51:27 +0000

    add correct file format

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -4,13 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
+/**
+ * hash_node_s - node of hash value to avoid collision
+ *
  * @key : The key, string
- * The key is unique in the HashTable
- *
  * @value : The value corresponding to a key
- *
  * @next : A pointer to the next node of the list
+ *
+ * Description: This is a node to a value with each node being a unique key
  */
 typedef struct hash_node_s
 {
@@ -19,17 +20,24 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
-/*
- * @size: The size of the array
+/**
+ * hash_table_s - hash table array structure
  *
+ * @size: The size of the array
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
+ *
+ * Description: Each cell of this array is a pointer to the first node of a
+ * linked list, because we want our HashTable to use a Chaining collision
+ * handling
  */
 typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+/* Functions */
+hash_table_t *hash_table_create(unsigned long int size);
+hash_node_t *add_node(hash_node_t **head);
 
 #endif
