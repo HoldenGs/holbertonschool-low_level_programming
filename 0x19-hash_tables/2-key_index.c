982edaaf9ba72a99408b7c601b81b550e8c43319
Holden Grissett <holdeng96@gmail.com>
2017-02-04 21:50:07 +0000

    fix betty documentation

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - find the index of the hash value for @key
+ *
+ * @key: key string
+ * @size: size of hash table array
+ *
+ * Return: index of @key's hash in hash table array
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
