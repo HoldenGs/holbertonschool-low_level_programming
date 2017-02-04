c669a417a68bb788e4a477b44793e8f623ced2d1
Holden Grissett <holdeng96@gmail.com>
2017-02-04 21:42:02 +0000

    add size checking

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -5,7 +5,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int hash;
 
 	hash = hash_djb2(key);
-	if (size > 0)
+	if (size != 0)
 		return (hash % size);
 	return (0);
 }
