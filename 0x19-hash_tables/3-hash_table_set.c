18e95f0bec3efc837b0f261037b70b5e9c9424e4
Holden Grissett <holdeng96@gmail.com>
2017-02-05 22:32:41 +0000

    fix comparison bug

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	unsigned long int index;
 
-	if (key == NULL || key == "")
+	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if ((node = key_exists(&ht->array[index], key)) != NULL)
