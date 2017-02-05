77f82cd57f7eebaf276419755cf9d8f2fbc4700a
Holden Grissett <holdeng96@gmail.com>
2017-02-05 22:42:45 +0000

    check for NULL value

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	unsigned long int index;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if ((node = key_exists(&ht->array[index], key)) != NULL)
