f5e0284db815eb6ddd590295dd9a803e7168d5c0
Holden Grissett <holdeng96@gmail.com>
2017-02-21 03:02:00 +0000

    add documentation for @prev perameter

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -10,6 +10,7 @@
  * struct listint_s - Singly linked list node
  *
  * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
