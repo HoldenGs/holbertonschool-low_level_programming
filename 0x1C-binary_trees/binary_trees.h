6f24eef942324a01bedf634540abf520b7ac4b77
Holden Grissett <holdeng96@gmail.com>
2017-03-14 00:39:55 +0000

    0 prototype

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -25,5 +28,6 @@ typedef struct binary_tree_s heap_t;
 
 /** Functions **/
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
