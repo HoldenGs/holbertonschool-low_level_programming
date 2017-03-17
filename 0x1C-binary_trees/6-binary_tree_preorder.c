75e651cb6d07ccf86a2d40daa2f5b8965bfc9127
Holden Grissett <holdeng96@gmail.com>
2017-03-17 02:48:16 +0000

    current is a constant type

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -11,7 +11,7 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *current;
+	const binary_tree_t *current;
 
 	current = tree;
 	if (current == NULL)
