7b916416f41144cb1c853d08f3aa0f39caad2822
Holden Grissett <holdeng96@gmail.com>
2017-03-27 21:01:41 +0000

    add NULL node checking

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node, *oldnode;
 
 	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (NULL);
 	node->n = value;
 	node->parent = parent;
 	oldnode = NULL;
